7d3287ec3edd25f1aebe6a1c429c71b6d39fc5f7
276525649@qq.com <276525649@qq.com>
2019-01-23 09:33:44 +0800

    1

--- src/components/RightContent/allCustomer.vue
+++ src/components/RightContent/allCustomer.vue
@@ -1,8 +1,12 @@
 <template>
-
+  <div class="allCustomer">
+    所有客户
+  </div>
 </template>
 <script type="text/ecmascript-6">
-  export default {}
+export default {
+  name: 'allCustomer'
+}
 </script>
 <style>
 
