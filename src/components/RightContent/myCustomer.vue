4580b12de32cff4d9360b8f9fec5a29413a289e6
276525649@qq.com <276525649@qq.com>
2019-01-28 15:49:18 +0800

    xc

--- src/components/RightContent/myCustomer.vue
+++ src/components/RightContent/myCustomer.vue
@@ -3,15 +3,31 @@
     <div class="search_info oh">
       <div class="col-xs-12">
         <div class="row">
-          <searchTxt></searchTxt>
-          <div class="startsearch searchbx">
-            <el-button type="primary" round>搜索</el-button>
-          </div>
+          <searchTxt ref="searchpro"></searchTxt>
         </div>
       </div>
     </div>
-    <div class="customerList mt30">
-      显示客户列表
+    <div class="customerList">
+      <div class="cantainer">
+        <el-table style="width: 100%;"
+         :data="customerdata">
+        <el-table-column label="编号" prop="code" width="">
+        </el-table-column>
+        <el-table-column label="客户名称" prop="name" width="">
+        </el-table-column>
+        <el-table-column label="国家" prop="pk_country" width="">
+        </el-table-column>
+        <el-table-column label="地区" prop="pk_areacl" width="">
+        </el-table-column>
+        </el-table>
+        <el-pagination
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-size="pagesize"
+          layout="total, prev, pager, next, jumper"
+          :total="customerdata.length">
+        </el-pagination>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +36,20 @@ import searchTxt from '../Util/customername'
 export default {
   data () {
     return {
+      customerdata: [],
+      currentPage: 1,
+      pagesize: 10
+    }
+  },
+  methods: {
+    fatherMethod (cusname, saleorg, psndoc) {
+      this.$http.get('/api/customer').then(res => {
+        res = res.body.customer
+        this.customerdata = res
+      })
+    },
+    handleCurrentChange: function (currentPage) {
+      this.currentPage = currentPage
     }
   },
   components: {
@@ -35,6 +65,14 @@ export default {
     float: left;
     max-width: 250px;
     margin-left: 20px;
+  }
+  .customerList{
+    position: absolute;
+    top:80px;
+    left:20px;
+    right:20px;
+    bottom:20px;
+    overflow-y: scroll;
   }
 
 </style>
